e47dc899cbe4bcdb2dd449b135c0803b9a93a41d
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2017-09-15 19:03:17 +0900

    The coding guideline is created.

--- frontend/webRoot/resources/vueObjects/vueMainHeader.js
+++ frontend/webRoot/resources/vueObjects/vueMainHeader.js
@@ -1,7 +1,19 @@
-Vue.component('vue-main-header', {
-  template: '#vueMainHeaderTemplate'
-});
+// Declare vue variable.
+var vueMainHeader;
 
-new Vue({
-  el: '#vueMainHeaderComponent',
+// ajax call to read html template and generate a vue object.
+$.ajax({
+  url:'/resources/vueObjects/vueMainHeader.html',
+  success:function(htmlCode){
+    // Register global component
+    Vue.component('vue-main-header', {
+      template : htmlCode
+    });
+
+    // Generate vue object.
+    // Edit below code to modify the vue object.
+    vueMainHeader = new Vue({
+      el: '#vueMainHeaderComponent',
+    });
+  }
 });
